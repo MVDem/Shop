a6695df23c0af798cc961c6df066f1d8e451bcd8
MVDem <m9602598255@gmail.com>
2022-07-11 13:42:50 +0300

    added card display using JS

---
+++ js/API.js
@@ -0,0 +1,54 @@
+export default {
+  fetch() {
+    return [
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product1',
+        price: 52,
+        discount: 5,
+      },
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product2',
+        price: 52,
+        discount: 5,
+      },
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product3',
+        price: 52,
+        discount: 5,
+      },
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product4',
+        price: 52,
+        discount: 5,
+      },
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product5',
+        price: 52,
+        discount: 5,
+      },
+      {
+        title: "ellery x m'o capsule",
+        description:
+          'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        image: 'product6',
+        price: 52,
+        discount: 5,
+      },
+    ];
+  },
+};
